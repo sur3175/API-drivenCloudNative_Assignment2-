ee34dadbd348922959b8bc443bdda0f80d04f180
sur3175 <32289870+sur3175@users.noreply.github.com>
2026-08-08 10:45:54 +0530

    Added client in generate_text

--- src/text_generation.py
+++ src/text_generation.py
@@ -1,8 +1,7 @@
 # This section of code will cover text generation task number 5.
 # And we are going to use Open AI API's to comeplte this task
-from config import client
 
-def generate_text(prompt):
+def generate_text(client, prompt):
     response = client.responses.create(
         model = "gpt-4o-mini",
         input = prompt
