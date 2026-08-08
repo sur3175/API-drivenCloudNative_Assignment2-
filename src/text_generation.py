874527e20a9b5cab7218f7f6b2f93a803ef6ab7b
sur3175 <32289870+sur3175@users.noreply.github.com>
2026-08-08 11:01:25 +0530

    Added new argument in function - provider
    This provider arument help us to identify whether we are using hf or openai

--- src/text_generation.py
+++ src/text_generation.py
@@ -1,9 +1,25 @@
-# This section of code will cover text generation task number 5.
-# And we are going to use Open AI API's to comeplte this task
+# This section create image base dupon prompt
+import base64
 
-def generate_text(client, prompt):
-    response = client.responses.create(
-        model = "gpt-4o-mini",
-        input = prompt
-    )
-    return response.output_text
+from pyarrow import null
+
+def generate_image(client, prompt: str, provider):
+    if provider == "openai":
+        response = client.images.generate (
+            model="gpt-image-1",
+            prompt=prompt
+        )
+        if response.data is None or len(response.data) == 0:
+            raise RuntimeError("No image was returned.")
+        
+        image_base64 = response.data[0].b64_json
+
+        if image_base64 is None:
+            raise RuntimeError("The generated image does not contain base64 data.")
+
+        image_bytes = base64.b64decode(image_base64)
+        return image_bytes
+    elif provider == "hf":
+        return null
+    else:
+        return null 
