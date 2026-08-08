0c17a2a4f3021d94b59ebdeee918fc897dea33ef
sur3175 <32289870+sur3175@users.noreply.github.com>
2026-08-08 11:02:02 +0530

    Update text_generation.py

--- src/text_generation.py
+++ src/text_generation.py
@@ -1,25 +1,17 @@
-# This section create image base dupon prompt
-import base64
+# This section of code will cover text generation task number 5.
+# And we are going to use Open AI API's to comeplte this task
 
 from pyarrow import null
 
-def generate_image(client, prompt: str, provider):
+
+def generate_text(client, prompt, provider):
     if provider == "openai":
-        response = client.images.generate (
-            model="gpt-image-1",
-            prompt=prompt
+        response = client.responses.create(
+            model = "gpt-4o-mini",
+            input = prompt
         )
-        if response.data is None or len(response.data) == 0:
-            raise RuntimeError("No image was returned.")
-        
-        image_base64 = response.data[0].b64_json
-
-        if image_base64 is None:
-            raise RuntimeError("The generated image does not contain base64 data.")
-
-        image_bytes = base64.b64decode(image_base64)
-        return image_bytes
+        return response.output_text
     elif provider == "hf":
         return null
     else:
-        return null 
+        return null
